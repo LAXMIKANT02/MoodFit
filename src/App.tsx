8be377917cb2cb0ad3fbbc84acf2af63a55ba79b
Pradyumna <pradyu.dp@gmail.com>
2025-09-12 18:07:23 +0530

    Start

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import Dashboard from './pages/Dashboard';
 import Logs from './pages/Logs';
 import Settings from './pages/Settings';
+import FitnessDetection from './pages/FitnessDetection';
+import PostureDetection from './pages/PostureDetection';
+import EmotionDetection from './pages/EmotionDetection';
 
 function App() {
   return (
@@ -14,6 +18,9 @@ function App() {
           <Route path="dashboard" element={<Dashboard />} />
           <Route path="logs" element={<Logs />} />
           <Route path="settings" element={<Settings />} />
+          <Route path="fitness-detection" element={<FitnessDetection />} />
+          <Route path="posture-detection" element={<PostureDetection />} />
+          <Route path="emotion-detection" element={<EmotionDetection />} />
         </Route>
       </Routes>
     </Router>
